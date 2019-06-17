8fa0a16f45511932336d50aee371ac673bcd72cf
HenrichsNiko <horstii@t-online.de>
2019-06-17 09:30:29 +0200

    Initial commit

---
+++ PHP/Index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <link rel="stylesheet" href="../CSS/styleAllg.css">
+</head>
+<body>
+
+<!--
+ 3.1: - Navgiationsstruktur
+      - Benamung
+      - Layout
+-->
+
+<div class="container">
+    <div class="header-container">
+        <a class="Logo" href="Index.php">
+            <img src="../CSS/img/Hotel.jpg" height="130" alt="Hotel-grp5">
+        </a>
+        <nav>
+            <ul class="clearfix">
+                <li><a href="Index.php">Startseite</a></li>
+                <li><a href="Zimmerauswahl">Zimmerauswhal</a></li>
+                <li><a href="ÜberUns">Über Uns</a></li>
+                <li><a href="Zimmerauswahl">Zimmerauswhal</a></li>
+                <li><a href="Zimmerauswahl">Zimmerauswhal</a></li>
+                <li><a href="Zimmerauswahl">Zimmerauswhal</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <!---
+    Button zum Registrieren / Login
+    -->
+
+    <div class="body-container">
+        <section class="body">
+            <h1>Hotelhomepage Gruppe 5</h1>
+            <a class="button" href="Login.php">Registrieren</a>
+            <a class="button" href="Registrieren.php">Login</a>
+        </section>
+
+        <div class="text-container">
+            <article>Um Zimmer reservieren zu können, <br>
+                müssen sie sich anmelden/registrieren.</article>
+        </div>
+    </div>
+
+    <!---
+    - Footer dort werden Namen und Gruppe aufgelistet
+    -->
+    <div class="footer-container">
+        <footer>Gruppe 5</footer>
+        <footer>Henrichs Niko, Frechen Kai, Naumann Marcel, Niehaves Lennart</footer>
+    </div>
+
+</div>
+
+</body>
+</html>
