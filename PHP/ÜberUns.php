a4b46220d968bd2eace0ddc1a48871f5c5b5a032
Kai96 <51920110+Kai96@users.noreply.github.com>
2019-07-02 01:37:49 +0200

    Add files via upload

--- PHP/ÜberUns.php
+++ PHP/ÜberUns.php
@@ -33,24 +33,33 @@
     <div class="body-container">
 
         <div class="introduction-text">
-        <h>Herzlich willkommen</h>
+            <center><h>Herzlich willkommen</h>
             <p>
                 Unser Hotel mit Meerblick bietet allen Besuchern ein unvergessliches Erlebnis.<br>
                 Durch eine schöne Lage in direkter Strandnähe ist ein toller Urlaub garantiert!<br>
-                Wir wünschen Ihnen einen schönen Urlaub.
+                Wir wünschen Ihnen einen schönen Urlaub in unserem 5-Sterne Hotel.<br><br>
             </p>
+            <h2>Unsere Geschichte:</h2>
+            <p> Seit vielen Jahren waren wir auf der Suche nach dem perfekten Ort für unser Hotel.<br>
+                    In diesen vielen Jahren haben wir viele Orte ausprobiert und uns entschlossen unser Hotel<br>
+                    in Bocholt zu starten. Der Ort war der perfekte Start für eine erfolgreiche Erfolgsgeschichte<br>
+                    eines mit viel Liebe erbauten Hotels. Der Name Serenity kam durch de Besuch von Joss Whedon,<br>
+                    den Direktor des Films Serenity aus dem Jahr 2005. Er war ein großartiger Mann dem wir viel<br>
+                    zu verrdanken haben. Aus diesem Grund haben wir beschlossen diesem großartigem Film alle Ehre<br>
+                    zu machen und unser Hotel danach benennen.<br>
+                </p></center>
 
         </div>
 
         <section class="body">
             <h1>Hotelhomepage Gruppe 5</h1>
-            <a class="button" href="Login.php">Registrieren</a>
-            <a class="button" href="Login.php">Login</a>
+            <center><a class="button" href="Login.php">Login</a></center><br>
+            <center><a class="button" href="Registrieren.php">Registrieren</a></center>
         </section>
 
         <div class="text-container">
-            <article>Um Zimmer reservieren zu können, <br>
-                müssen sie sich anmelden/registrieren.</article>
+            <center><article><br>
+                    Um Zimmer reservieren zu können, müssen sie sich anmelden/registrieren.</article></center>
         </div>
     </div>
 
