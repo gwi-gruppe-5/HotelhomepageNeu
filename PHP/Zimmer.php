78a183846b36cab8528a90239f0c5b244af82b98
lniehaves <32952203+lniehaves@users.noreply.github.com>
2019-07-02 18:57:07 +0200

    buchen, suchen, zimmer auswahl angepasst und design

--- PHP/Zimmer.php
+++ PHP/Zimmer.php
@@ -31,7 +31,7 @@
 
 
         <h2>Einzelzimmer</h2>
-        <img src="../CSS/img/Zimmer/Zimmer1.PNG" width="40%">
+            <a href="zimmer_suchen.php?zimmer=1"><img src="../CSS/img/Zimmer/Zimmer1.PNG" width="40%"></a>
         <p>Unser Einzelzimmer ist ideal für kurzfristige Übernachtungen<br>
 
         </p>
@@ -40,21 +40,21 @@
     <div>
     <div style="margin-top: 30%">
         <h2>Komfortzimmer</h2>
-        <img src="../CSS/img/Zimmer/Zimmer2.PNG" width="40%">
+        <a href="zimmer_suchen.php?zimmer=2"> <img src="../CSS/img/Zimmer/Zimmer2.PNG" width="40%"></a>
         <p>Unser Komfortzimmer ist ideal für stadtnahe Übernachtungen oder romatische Wochenenden zu Zweit</p>
         <br>
     </div>
 
     <div style="margin-top: 30%">
         <h2>Luxuszimmer</h2>
-        <img src="../CSS/img/Zimmer/Zimmer3.PNG" width="40%">
+        <a href="zimmer_suchen.php?zimmer=3"><img src="../CSS/img/Zimmer/Zimmer3.PNG" width="40%"></a>
         <p>Unser Luxuszimmer bietet viel Raum für ein entspanntes Verweilen.</p>
         <br>
     </div>
 
     <div style="margin-top: 30%">
         <h2>Juniorsuite</h2>
-        <img src="../CSS/img/Zimmer/Zimmer4.PNG" width="40%">
+        <a href="zimmer_suchen.php?zimmer=4"><img src="../CSS/img/Zimmer/Zimmer4.PNG" width="40%"></a>
         <p>Junior Suite - Viel Platz und Luxus für ein gemütliches, großzügiges Arrangement</p>
         <br>
     </div>
