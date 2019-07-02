9826a3ef2bc0ea5814caef1a541d19dc3047ac67
lniehaves <32952203+lniehaves@users.noreply.github.com>
2019-07-02 16:29:16 +0200

    buchen 30000

--- PHP/ÜberUns.php
+++ PHP/ÜberUns.php
@@ -48,7 +48,8 @@
                     zu verrdanken haben. Aus diesem Grund haben wir beschlossen diesem großartigem Film alle Ehre<br>
                     zu machen und unser Hotel danach benennen.<br>
                 </p></center>
-
+            <h2>Anfahrt:</h2>
+           <center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19719.15420464904!2d6.628283339550781!3d51.844626549999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf461a835238b4f4d!2sWestf%C3%A4lische+Hochschule%2C+Campus+Bocholt!5e0!3m2!1sde!2sde!4v1562072175231!5m2!1sde!2sde" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></center>
         </div>
 
         <section class="body">
