f776b800c9bab648588c86b044979986dd100cf6
lniehaves <32952203+lniehaves@users.noreply.github.com>
2019-07-02 18:19:50 +0200

    buchen buchen

--- PHP/Buchen.php
+++ PHP/Buchen.php
@@ -29,25 +29,51 @@
 
    <div class="body-container">
        <?php
+
        if(isset($_GET['auswahl']))
        {
            $zimmerNr = $_GET['auswahl'];
-           $date1 = $_POST['date1'];
-           $date2 = $_POST['date2'];
-       }
 
+       }
+       include("../PHP/include/DBVerbindung.php");
        if(isset($_GET['buchen']))
        {
-           if{}
+
+           if($_GET['buchen']==1)
+           {
+
+               $date2 = $_POST['date2'];
+               $date1 = strtotime($_POST["date1"]);
+               $date1 = date('Y-m-d ', $date1);
+               $date2 = strtotime($_POST["date2"]);
+               $date2 = date('Y-m-d ', $date2);
+               $anzahl =$_POST['anzahl'];
+
+               $cDate = date("Y-m-d");
+               echo $date1;
+               $sql = "INSERT INTO `buchung` (`BuchungID`, `BuchungDatum`, `AnreiseDatum`, `AbreiseDatum`, `AnzahlPersonen`, `Zimmer_ZimmerID`, `Kunde_KundeID`) VALUES (NULL,'$cDate','$date1' , '$date2',$anzahl , $zimmerNr, '3')";
+
+
+               if($conn->query($sql)==true)
+               {
+                   echo "Zimmer gebucht vom ".$date1." bis zum ".$date2;
+               }
+                else{
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+
+           }
        }
        ?>
    </div>
 
-    <form action="?buchen=1" method="post">
+    <form action="?buchen=1&auswahl=1" method="post">
         Anreisedatum
         <input type="date" name="date1">
         Abreisedatum
         <input type="date" name="date2"><br>
+        Anzahl Personen
+        <input type="text" name="anzahl"><br>
         <input type="submit" value="Buchen">
 
     </form>
