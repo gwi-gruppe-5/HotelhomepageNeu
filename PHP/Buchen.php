5b16dd04a3c74f1e956f01e5ad824698961598ac
lniehaves <32952203+lniehaves@users.noreply.github.com>
2019-07-02 14:44:33 +0200

    suchen buchen

--- PHP/Buchen.php
+++ PHP/Buchen.php
@@ -16,57 +16,36 @@
 
 <div class="container">
     <div class="header-container">
+
         <?php
         include("../PHP/include/Header.php");
         ?>
+
     </div>
 
+    <!---
+    Button zum Registrieren / Login
+    -->
 
+   <div class="body-container">
+       <?php
+       if(isset($_GET['auswahl']))
+       {
+           $zimmerNr = $_GET['auswahl'];
+           $date1 = $_GET['d1'];
+           $date2 = $_GET['d2'];
+       }
+       ?>
+   </div>
+
+    <!---
+    - Footer dort werden Namen und Gruppe aufgelistet
+    -->
     <?php
-    //  DB Verbindung siehe verbindung.php
-    include("../PHP/include/DBVerbindung.php");
-    if (isset($_GET['suchen']))
-    {
-        $date1 = $_POST['date1'];
-        $date2 = $_POST['date2'];
-        $zimmerID = $_POST['zimmer'];
-
-    }
-
+    include("../PHP/include/Footer.php");
     ?>
-
-
-    <form action="?suchen=1" method="post">
-        Anreisedatum
-        <input type="date" name="date1">
-        Abreisedatum
-        <input type="date" name="date2"><br>
-        <input type="radio" name="zimmer" value="1" checked>Einzelzimmer <br>
-        <input type="radio" name="zimmer" value="2"> Komfortzimmer<br>
-        <input type="radio" name="zimmer" value="3">Luxuszimmer<br>
-        <input type="radio" name="zimmer" value="4">Juniorsuite<br>
-        <input type="radio" name="zimmer" value="5">Suitesuperior<br>
-        <input type="submit" value="Zimmer suchen">
-
-
-    </form>
-
-
-    <div class="freie-zimmer">
-        <?php
-            
-        ?>
-
-
-    </div>
-
-    <div class="footer-container">
-        <?php
-        include("../PHP/include/Footer.php");
-        ?>
-    </div>
 
 </div>
 
 </body>
-
+</html>
